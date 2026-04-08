a89db34d20d51d657917d1513a0d3908bb13e8d4
Ricardo Alcantara <alcantararicardo04@gmail.com>
2026-04-07 23:41:24 -0500

    Agregando repository de la entidad categoria en repository

---
+++ minimarket/src/main/java/com/rlaal/minimarket/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.rlaal.minimarket.repository;
+
+import com.rlaal.minimarket.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
+}
